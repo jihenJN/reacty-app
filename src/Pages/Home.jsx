932145e02e2b06b2e9990c41ffff35828b503f2a
jihenJN <jihenjihen23@gmail.com>
2025-08-10 12:16:59 +0100

    feat: fetch api data using axios

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,25 @@
-function Home(){
-   return(
-    <h1> Home PAGE</h1>
-   ) 
+import { useEffect, useState } from "react";
+import axios from "axios";
+function Home() {
+   const [users,setUsers]=useState([])
+  async function fetchData() {
+  
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+ 
+    setUsers(data)
+ 
+   
+  
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return(<div>
+   { users.length >0 
+   ?<h1>{users.length>0 ? users[0].username:null  }</h1>
+   : <h1>loading ...</h1>   }</div> )
+  
 }
 
-export default Home
+export default Home;
